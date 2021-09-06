82b135dd3b0b78a2058baa87c2c5d767ef00a829
BrunoGambaRocha <bruno.roch@gmail.com>
2021-09-06 16:12:00 -0300

    Project created

---
+++ backend/src/test/java/br/com/brunoti/cursos/devsuperior/sds4/dsvendas/DsvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.brunoti.cursos.devsuperior.sds4.dsvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
